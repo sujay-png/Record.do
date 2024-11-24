b46ee4c1e4af8305c35fef8643c6455ec1a138c5
sujay.png <sujaykumarp00@gmail.com>
2024-11-24 16:06:48 +0530

    changed folder name

--- app.js
+++ app.js
@@ -13,35 +13,35 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.get('/', (req, res) => {
-    fs.readdir(`records`, (err, files) => {
+    fs.readdir(`admin-input`, (err, files) => {
         if (err) return res.status(500).send(err);
         res.render("index.ejs", { files: files });
     });
 });
 
 app.get('/edit/:filename', (req, res) => {
-    fs.readFile(`./records/${req.params.filename}`, "utf-8", (err, fileData)=>{
+    fs.readFile(`./admin-input/${req.params.filename}`, "utf-8", (err, fileData)=>{
         if (err) return res.status(500).send(err.message);
         res.render("edit.ejs", {fileData, fileName: req.params.filename})
     })
 });
 
 app.get('/delete/:filename', (req, res) => {
-    fs.unlink(`./records/${req.params.filename}`, (err)=>{
+    fs.unlink(`./admin-input/${req.params.filename}`, (err)=>{
         if (err) return res.status(500).send(err.message);
         res.redirect("/");
     })
 });
 
 app.get('/hisaab/:filename', (req, res) => {
-    fs.readFile(`./records/${req.params.filename}`, "utf-8", (err, fileData)=>{
+    fs.readFile(`./admin-input/${req.params.filename}`, "utf-8", (err, fileData)=>{
         if (err) return res.status(500).send(err.message);
         res.render("hisaab.ejs", {fileData, fileName: req.params.filename})
     })
 });
 
 app.post('/update/:filename', (req, res) => {
-    fs.writeFile(`./records/${req.params.filename}`, `${req.body.content}`, (err)=>{
+    fs.writeFile(`./admin-input/${req.params.filename}`, `${req.body.content}`, (err)=>{
         if (err) return res.status(500).send(err.message);
         res.redirect("/");
     })
@@ -54,10 +54,10 @@ app.get('/create', (req, res) => {
 app.post('/createhisaab', (req, res) => {
     let currentDate = new Date();
     let date = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`; 
-    const recordsDir = path.join(__dirname, 'records');
-    const filePath = path.join(recordsDir, date);
+    const admininputDir = path.join(__dirname, 'admin-input');
+    const filePath = path.join(admininputDir, date);
 
-    fs.mkdir(recordsDir, { recursive: true }, (err) => {
+    fs.mkdir(admininputDir, { recursive: true }, (err) => {
         if (err) return res.status(500).send(err.message);
         
         fs.writeFile(filePath, req.body.content, (err) => {
